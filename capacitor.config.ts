d5048e0947648f56259887e15dcd6acd365d09a0
maiborra2 <troveaig@gmail.com>
2023-03-16 11:43:55 +0100

    commit antes de publicar

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
+  appId: 'com.videomani.app',
   appName: 'Videomani',
   webDir: 'www',
   bundledWebRuntime: false
